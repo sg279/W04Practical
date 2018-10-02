29014a0f1565b4ff213b103fbc15a40e8a25406f
sg279 <54mgold@gmail.com>
2018-10-02 10:39:41 +0100

    Initial

---
+++ src/TimeStamp.java
@@ -0,0 +1,23 @@
+
+/**
+ * Simple example of a timestamp.
+ *
+ * Saleem Bhatti, https://saleem.host.cs.st-andrews.ac.uk/
+ * 18 Sep 2018
+ *
+ */
+
+import java.util.*;
+import java.text.*;
+
+public class TimeStamp {
+
+    public static void main(String[] args) {
+        Date               d = new Date();
+        String            df = new String("yyyy-MM-dd_HH:mm:ss.SSS");
+        SimpleDateFormat sdf = new SimpleDateFormat(df);
+        String             s = sdf.format(d);
+
+        System.out.println("Timestamp : " + s);
+    }
+}
